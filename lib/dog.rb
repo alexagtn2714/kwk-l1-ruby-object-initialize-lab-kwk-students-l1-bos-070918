34c9984319c5649aac835edd49fdf0613e355b25
alexagtn2714 <alexagaton@hotmail.com>
2018-07-13 15:22:01 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+class Dog 
+  attr_reader :name, :breed 
+  
+  def initialize(name,breed="Mutt")
+    @name = name 
+    @breed = breed 
+  end 
+  
+end 
+
+fido = Dog.new("Fido", "Pug")
+
+puts fido.name 
+puts fido.breed 
